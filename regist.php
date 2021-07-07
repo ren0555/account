6776f11fed5135999cfffe1cd89f3468ec1ebc50
unknown <renta.s.1216@gmail.com>
2021-07-08 02:55:36 +0900

    7/8

---
+++ regist.php
@@ -0,0 +1,129 @@
+<html>
+    <head>
+        <title>アカウント登録画面</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <header>
+            <h1>ナビゲーションバー</h1>
+        </header>
+        <main>
+            <form method="post"  action="XXX" name="XXX" ></form>
+            <div>
+                <label>名前(姓)</label><br>
+                <input type="text" class="text" size="XXX" name="name1">
+            </div>
+            
+            <div>
+                <label>名前(名)</label><br>
+                <input type="text" class="text" size="XXX" name="name2">
+            </div>
+            
+            <div>
+                <label>カナ(姓)</label><br>
+                <input type="text" class="text" size="XXX" name="name3">
+            </div>
+            
+            <div>
+                <label>カナ(名)</label><br>
+                <input type="text" class="text" size="XXX" name="name4">
+            </div>
+            
+            <div>
+                <label>メールアドレス</label><br>
+                <input type="text" class="text" size="XXX" name="mail">
+            </div>
+            
+            <div>
+                <label>パスワード</label><br>
+                <input type="text" class="text" size="XXX" name="password">
+            </div>
+            
+            <div>
+                <label>性別</label>
+                <input type="radio" size="XXX" name="seibetu">男
+                <input type="radio" size="XXX" name="seibetu">女
+            </div>
+            
+            <div>
+                <label>郵便番号</label>
+                <input type="text" class="text" size="XXX" name="yubin">
+            </div>
+            
+            <div>
+                <label>住所(都道府県)</label>
+                <select name="XXX">
+                    <option value="1">北海道</option>
+                    <option value="2">青森県</option>
+                    <option value="3">岩手県</option>
+                    <option value="4">宮城県</option>
+                    <option value="5">秋田県</option>
+                    <option value="6">山形県</option>
+                    <option value="7">福島県</option>
+                    <option value="8">茨城県</option>
+                    <option value="9">栃木県</option>
+                    <option value="10">群馬県</option>
+                    <option value="11">埼玉県</option>
+                    <option value="12">千葉県</option>
+                    <option value="13">東京都</option>
+                    <option value="14">神奈川県</option>
+                    <option value="15">新潟県</option>
+                    <option value="16">富山県</option>
+                    <option value="17">石川県</option>
+                    <option value="18">福井県</option>
+                    <option value="19">山梨県</option>
+                    <option value="20">長野県</option>
+                    <option value="21">岐阜県</option>
+                    <option value="22">静岡県</option>
+                    <option value="23">愛知県</option>
+                    <option value="24">三重県</option>
+                    <option value="25">滋賀県</option>
+                    <option value="26">京都府</option>
+                    <option value="27">大阪府</option>
+                    <option value="28">兵庫県</option>
+                    <option value="29">奈良県</option>
+                    <option value="30">和歌山県</option>
+                    <option value="31">鳥取県</option>
+                    <option value="32">島根県</option>
+                    <option value="33">岡山県</option>
+                    <option value="34">広島県</option>
+                    <option value="35">山口県</option>
+                    <option value="36">徳島県</option>
+                    <option value="37">香川県</option>
+                    <option value="38">愛媛県</option>
+                    <option value="39">高知県</option>
+                    <option value="40">福岡県</option>
+                    <option value="41">佐賀県</option>
+                    <option value="42">長崎県</option>
+                    <option value="43">熊本県</option>
+                    <option value="44">大分県</option>
+                    <option value="45">宮崎県</option>
+                    <option value="46">鹿児島県</option>
+                    <option value="47">沖縄県</option>
+                </select>
+            </div>
+            
+            <div>
+                <label>住所(市区町村)</label>
+                <input type="text"class="text" size="XXX" name="sikutyouson">
+            </div>
+            <div>
+                <label>住所(番地)</label>
+                <input type="text"class="text" size="XXX" name="banti">
+            </div>
+
+            <div>
+                <label>アカウント権限</label>
+                <select name="XXX">
+                    <option>一般</option>
+                    <option>管理者</option>
+                </select>
+            </div>
+            
+            <div>
+                <input type="button" value="確認する">
+            </div>
+        </main>
+    </body>
+
+</html>
